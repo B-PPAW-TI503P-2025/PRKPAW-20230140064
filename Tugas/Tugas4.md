2ca4676e41881a6190fd4739b8d8b3fc26e3b1ef
F4KHR111 <fakhriwirdiyan05@gmail.com>
2025-10-24 10:05:14 +0700

    Tugas4

---
+++ Tugas/Tugas4.md
@@ -0,0 +1,10 @@
+1.Tampilan End Point CheckIn:
+   ![Check In](./ss4/CheckIn.png)
+2.Tampilan End Point CheckIn:
+   ![Check In Database](./ss4/CheckInDB.png)
+3.Tampilan End Point ChekOut:
+   ![Check Out](./ss4/Checkout.png)
+4.Tampilan End Point Database:
+   ![Check Out Database](./ss4/CheckOutDB.png)
+5.Tampilan End Point ReportsDaily:
+   ![Reports Daily](./ss4/ReportDaily.png)
--- pawpraktikum_node_20230140064/controllers/reportController.js
+++ pawpraktikum_node_20230140064/controllers/reportController.js
@@ -1,8 +1,21 @@
-const presensiRecords = require("../data/presensiData");
-exports.getDailyReport = (req, res) => {
-  console.log("Controller: Mengambil data laporan harian dari array...");
-  res.json({
-    reportDate: new Date().toLocaleDateString(),
-    data: presensiRecords,
-  });
+const { Presensi } = require("../models");
+
+exports.getDailyReport = async (req, res) => {
+  try {
+    console.log("Controller: Mengambil data laporan harian dari database...");
+
+    // Ambil semua data presensi dari database
+    const presensiRecords = await Presensi.findAll();
+
+    // Ubah ke format JSON agar bisa dikirim ke client
+    const plainRecords = presensiRecords.map((r) => r.toJSON());
+
+    res.json({
+      reportDate: new Date().toLocaleDateString(),
+      data: plainRecords,
+    });
+  } catch (error) {
+    console.error("Error mengambil data laporan harian:", error);
+    res.status(500).json({ message: "Terjadi kesalahan pada server" });
+  }
 };
